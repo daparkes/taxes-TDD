8ef99e78a7951fccc5e674ae3975561a4c078360
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-12 10:41:45 +0000

    Fixed tests

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -1,25 +1,16 @@
 class TaxCalculator {
 
   def calculateTax(income: Int, incomeCaps: Array[Int], marginTaxRate: Array[Int]): Int = {
-    var stillTaxableIncome = income
     var totalTax = 0
-    for ( i <-incomeCaps.length to 1 by -1) {
-      var j = i
-      println(i)
-      var thisIncomeCap = incomeCaps(j)
-      var thisTaxRate = marginTaxRate(j-1)
-      while (thisIncomeCap > stillTaxableIncome) {
-        if (j>0) {
-          j -= 1
-          thisIncomeCap = incomeCaps(j)
-          thisTaxRate = marginTaxRate(j-1)
-        }
+    for ( i <- 1 until incomeCaps.length) {
+      if (income >= incomeCaps(i)) {
+        totalTax += (incomeCaps(i) - incomeCaps( i- 1)) / marginTaxRate(i - 1)
       }
-      println((stillTaxableIncome - thisIncomeCap))
-      totalTax += (stillTaxableIncome - thisIncomeCap) / thisTaxRate
+    }
+    if (income > incomeCaps(incomeCaps.length - 1)) {
+      totalTax += (income - incomeCaps(incomeCaps.length - 1)) / (100 / marginTaxRate(marginTaxRate.length - 1))
     }
     totalTax
-//    (income-incomeCaps(0)) / marginTaxRate(0)
   }
 
 }
