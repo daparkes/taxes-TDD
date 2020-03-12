c69dd56834e700c73fa3c482f448c94e2abadce1
Dan Parkes <dan.parkes15@gmail.com>
2020-03-12 11:18:55 +0000

    changed function to with with decimals

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 object Main extends App {
 
   val taxCalc = new TaxCalculator
-  taxCalc.calculateTax(5000, Array(1000, 4000), Array(10, 20))
+  taxCalc.calculateTax(50000, Array(1000, 10000, 40000), Array(0.1, 0.2, 0.3))
 
 }
