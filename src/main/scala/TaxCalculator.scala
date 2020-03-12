fe86083ff1b8461598e4582ca9dc953483da2aee
Johannes Bull <johannesniklasbull@gmail.com>
2020-03-12 10:12:46 +0000

    Tried to fix tests

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -1,7 +1,22 @@
 class TaxCalculator {
 
   def calculateTax(income: Int, incomeCaps: Array[Int], marginTaxRate: Array[Int]): Int = {
-    (income-incomeCaps(0)) / marginTaxRate(0)
+    var stillTaxableIncome = income
+    var totalTax = 0
+    for ( i <-incomeCaps.length - 1 to 0 by -1) {
+      var j = i
+      var thisIncomeCap = incomeCaps(j)
+      var thisTaxRate = marginTaxRate(j)
+      while (thisIncomeCap > stillTaxableIncome) {
+        j -= 1
+        thisIncomeCap = incomeCaps(j)
+        thisTaxRate = marginTaxRate(j)
+      }
+      println((stillTaxableIncome - thisIncomeCap))
+      totalTax += (stillTaxableIncome - thisIncomeCap) / thisTaxRate
+    }
+    totalTax
+//    (income-incomeCaps(0)) / marginTaxRate(0)
   }
 
 }
