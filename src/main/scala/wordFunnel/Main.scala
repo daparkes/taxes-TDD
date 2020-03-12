9dc10f8e53694149b0417684b8e9e333fba7dc3a
Dan Parkes <dan.parkes15@gmail.com>
2020-03-12 15:03:49 +0000

    half a bad solution

--- src/main/scala/wordFunnel/Main.scala
+++ src/main/scala/wordFunnel/Main.scala
@@ -3,6 +3,8 @@ package wordFunnel
 import scala.io.Source
 
 object Main extends App {
+  //val funnel = new FunnelWord
+ // println(funnel.findFunnelList("princesses"))
   val funnel = new FunnelWord
-  println(funnel.findFunnelList("princesses"))
+  funnel.funnelWord("princesses")
 }
